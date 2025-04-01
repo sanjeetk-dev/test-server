f1a07bca6a49698955d242f629658e6685a3512e
sanjeetk-dev <sanjeetk.dev@gmail.com>
2025-04-01 22:00:01 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import cors from "cors";
 import morgan from "morgan";
 dotenv.config()
 import errorHandler from "./middlewares/errorHandler.middleware.js";
+import {job} from './utils/cron.js'
 const app = express();
+
+job.start()
 
 app.use(express.json({ limit: "16kb" })); // JSON Parsing
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // URL-Encoded Data
--- src/utils/cron.js
+++ src/utils/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron"
 import nodemailer from "nodemailer"
 import https from "https"
-const job = new cron.CronJob("*/10 * * * *", async () => {
+export const job = new cron.CronJob("*/10 * * * *", async () => {
   
   https
     .get("", (res) => {
